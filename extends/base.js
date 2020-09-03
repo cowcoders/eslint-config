b502e7c33b88b7a507487c0d41b26c1b552e343c
raulanatol <raul@natol.es>
2020-09-03 10:39:29 +0100

    Update base config

--- extends/base.js
+++ extends/base.js
@@ -1,12 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   plugins: [
-    'no-only-tests',
+    'no-only-tests'
   ],
   rules: {
     'no-only-tests/no-only-tests': 'error',
-  },
+    'no-unused-vars': 'error',
+    'no-multiple-empty-lines': 'error',
+    'curly': [
+      2,
+      'all'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-multi-spaces': 'error',
+    'comma-dangle': 'error',
+    'no-var': 'error'
+  }
 };
