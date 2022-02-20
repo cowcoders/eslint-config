0c4a93a75ebb8de9cd8d3cb85a2b2f654d6f880b
raulanatol <raul@natol.es>
2022-02-20 12:51:13 +0000

    jsx_indent_props update

--- tests/component.tsx
+++ tests/component.tsx
@@ -3,13 +3,13 @@ import React, { FC } from 'react';
 export const Component: FC = () => {
   return <div>
     <h1 key="123"
-      aria-label="name">Hello World</h1>
+        aria-label="name">Hello World</h1>
   </div>;
 };
 
 const Simple: FC = () => {
   return <Component name="one"
-    value={10}/>;
+                    value={10}/>;
 };
 
 console.log(Simple);
