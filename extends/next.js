e1aa923e29899679cbbe8f6db9b7153289580d67
raulanatol <raul@natol.es>
2022-02-24 17:49:58 +0000

    Update next rules

--- extends/next.js
+++ extends/next.js
@@ -7,6 +7,9 @@ module.exports = {
     'jsx-quotes': 'error',
     'react/jsx-tag-spacing': ['error', { 'beforeSelfClosing': 'never' }],
     'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'import/no-anonymous-default-export': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/display-name': 2
   }
 };
--- next.js
+++ next.js
@@ -1,9 +1,8 @@
 module.exports = {
-  plugins: ['import', 'react-hooks'],
   extends: [
     './index.js',
     './extends/ts.js',
-    './extends/react.js',
-    './extends/next.js'
+    './extends/next.js',
+    './extends/jest.js'
   ]
 };
